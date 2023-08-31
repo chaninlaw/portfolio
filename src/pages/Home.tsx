8646873d457a228ea53a1cea4be1ac320096b225
Chanin Law <chaninlawlert@gmail.com>
2023-08-31 07:55:12 +0700

    add mesh

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Typography, theme } from 'antd'
+import MeshBox from '../components/MeshBox'
+import { Canvas } from '@react-three/fiber'
 
 interface Props {}
 
 const HomePage: React.FC<Props> = () => {
   const { colorTextLightSolid } = theme.useToken().token
 
-  return <Typography color={colorTextLightSolid}>home</Typography>
+  return (
+    <>
+      <Typography color={colorTextLightSolid}>home</Typography>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <MeshBox position={[-1.2, 0, 0]} />
+      </Canvas>
+    </>
+  )
 }
 
 export default HomePage
